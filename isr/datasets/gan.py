eb4d1fc8512f22bd64f95e260a6d84a47bcdd911
Peter Vrancx <peter@prwoler.io>
2020-05-27 15:24:11 +0100

    added discriminator dataset

---
+++ isr/datasets/gan.py
@@ -0,0 +1,42 @@
+from random import random
+
+from torchvision.datasets import VisionDataset
+
+from isr.datasets.isr import IsrDataset
+from isr.models.lightning_model import LightningIsr
+
+
+class DiscriminatorDataset(VisionDataset):
+    def __init__(
+            self,
+            wrapped_dataset: IsrDataset,
+            generator: LightningIsr,
+            transform=None,
+            target_transform=None
+    ):
+        super(DiscriminatorDataset, self).__init__(
+            wrapped_dataset.root,
+            transform=transform,
+            target_transform=target_transform
+        )
+        self._dataset = wrapped_dataset
+        self._generator = generator
+
+    def __len__(self):
+        return len(self._dataset)
+
+    def __getitem__(self, item):
+        lr_img, hr_img = self._dataset[item]
+
+        if random() < 0.5:
+            img = self._generator(lr_img)[0]
+            target = 0
+        else:
+            img = hr_img
+            target = 1
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        if self.transform is not None:
+            img = self.transform(img)
+        return img, target
